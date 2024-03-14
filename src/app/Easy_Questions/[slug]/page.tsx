b5c8ceff5348fb2d8373e36eee5deca4aa58d09b
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2024-03-14 10:26:05 -0400

    edited form in quiz

--- src/app/Easy_Questions/[slug]/page.tsx
+++ src/app/Easy_Questions/[slug]/page.tsx
@@ -3,14 +3,12 @@ import { EasyQA } from '@/Data';
 import { useParams } from 'next/navigation';
 import { Quiz } from '@/useClient/Quiz';
 
-/// This could be a static rendering GETSTATICPROPS once data is in a server. 
+/// This could be a static rendering GETSTATICPROPS once data is in a server.
 /// Data will be cached at build time and cach to a CDN
 export default function Page({ params }: { params: { slug: string } }) {
   const { index } = useParams();
 
   const questionIndex = parseInt(params.slug + 1);
 
-  return (
-<Quiz questions={EasyQA} questionIndex={questionIndex} />
-  );
+  return <Quiz questions={EasyQA} questionIndex={questionIndex} />;
 }
--- src/useClient/Quiz.tsx
+++ src/useClient/Quiz.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { Label } from "@/components/ui/label"; 
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Label } from '@/components/ui/label';
 import { z } from 'zod';
-import { zodResolver } from "@hookform/resolvers/zod"; 
-import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import {
   Form,
   FormControl,
@@ -11,7 +11,7 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
+} from '@/components/ui/form';
 import { Button } from '@/components/ui/button';
 
 interface QuestionType {
@@ -19,82 +19,61 @@ interface QuestionType {
   options: string[];
   correct_answer: string;
 }
-
 interface QuizProps {
   questions: QuestionType[];
   questionIndex: number;
 }
 
-interface RadioGroupDemoProps {
-  options: string[];
-}
-
 const FormSchema = z.object({
-  type: z.enum(["A", "B", "C", "D"], {
-    required_error: "You need to select a notification type.",
+  type: z.enum(['A', 'B', 'C', 'D'], {
+    required_error: 'The Answer selected is invalid. Please select a valid answer.',
   }),
-})
+});
 
-export function RadioGroupForm() {
+// This is the Quiz component that is exported to the page.
+export function Quiz({ questions, questionIndex }: QuizProps) {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
-  })
-} 
+  });
 
 
-const handleDataSubmit: SubmitHandler<z.infer<typeof FormSchema>> = (formData) => {
-  console.log(formData);
-};
-  
-
-export function RadioGroupDemo({ options }: RadioGroupDemoProps) {
- 
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    const formData = form.getValues();
+    console.log("hello world", formData);
+  }
 
   return (
-    <RadioGroup defaultValue="">
-      {options.map((option, index) => {
-
-        return (
-          <div key={index} className="flex items-center space-x-2">
-            <RadioGroupItem value={option} id={`r${index}`}  />
-            <Label htmlFor={`r${index}`}>{option}</Label>
-          </div>
-        );
-      })}
-    </RadioGroup>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='w-2/3 space-y-6'>
+        <FormField
+          control={form.control}
+          name='type'
+          render={({ field }) => (
+            <FormItem className='space-y-3'>
+              <FormLabel>{questions[questionIndex].question}</FormLabel>
+              <FormControl>
+                <RadioGroup defaultValue={field.value}
+                onValueChange={field.onChange}>
+                  {questions[questionIndex].options.map((option, index) => {
+                    return (
+                      <div key={index} className='flex items-center space-x-2'>
+                        <RadioGroupItem
+                          value={option.charAt(0).toUpperCase()}
+                          id={`r${index}`}
+                        />
+                        <Label htmlFor={`r${index}`}>{option}</Label>
+                      </div>
+                    );
+                  })}
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
   );
 }
-
-
-// This is the Quiz component that is exported to the page. 
-export function Quiz({ questions, questionIndex }: QuizProps) {
-  
-
-  const { 
-    register, 
-    handleSubmit, 
-    formState: { errors },
-  } = useForm({defaultValues: {radio:''}});
-  
-  
-
-  return (
-    <form onSubmit={handleSubmit(console.log)}>
-      <div>
-        {questions[questionIndex] && (
-          <div key={questionIndex}>
-            <h2>{questions[questionIndex].question}</h2>
-            <RadioGroupDemo options={questions[questionIndex].options} {...register("radio")} />
-          </div>
-        )}
-      </div>
-    <Button  type="submit">Submitt</Button>
-    </form>
-  );
-};
-
-
-
-
-
 
