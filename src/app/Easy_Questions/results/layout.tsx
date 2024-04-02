a6520313ca3a01f26be7a349611edacefc116e0b
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2024-04-02 17:29:38 -0400

    added prisma

---
+++ src/app/Easy_Questions/results/layout.tsx
@@ -0,0 +1,11 @@
+export default function QuizLayout({
+	children, 
+  }: {
+	children: React.ReactNode
+  }) {
+	return (
+	  <section className="flex justify-center items-center min-h-screen">
+		{children}
+	  </section>
+	)
+  }
---
+++ src/app/Easy_Questions/results/page.tsx
@@ -0,0 +1,18 @@
+'use client'; 
+
+
+
+export default function EasyQuestions() {
+
+
+	return (
+	  <>
+		
+		  <div>
+			<h1>Results</h1>
+
+		  </div> 
+	  </>
+	);
+  }
+  
