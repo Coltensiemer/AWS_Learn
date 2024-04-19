217569ade0e9c8dfb3cb0127a401d6f6daaafbf3
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2024-04-19 13:31:44 -0400

    commented out api route - may of been causing production error

--- src/app/api/get-quiz/route.ts
+++ src/app/api/get-quiz/route.ts
@@ -1,30 +1,30 @@
-import { PrismaClient } from '@prisma/client';
-import { QuestionType } from '../../../../prisma/dataTypes';
-import { NextResponse } from 'next/server';
+// import { PrismaClient } from '@prisma/client';
+// import { QuestionType } from '../../../../prisma/dataTypes';
+// import { NextResponse } from 'next/server';
 
-export async function GET() {
-    const prisma = new PrismaClient();
+// export async function GET() {
+//     const prisma = new PrismaClient();
 
-    try {
-        const data: QuestionType[] = await prisma.quiz.findMany({
-            where: {
-                OR: [
-                    { tag: 'EC2' },
-                    { tag: 'Lambda' }
-                ],
-            },
-            include: {
-                options: true,
-            },
-        });
+//     try {
+//         const data: QuestionType[] = await prisma.quiz.findMany({
+//             where: {
+//                 OR: [
+//                     { tag: 'EC2' },
+//                     { tag: 'Lambda' }
+//                 ],
+//             },
+//             include: {
+//                 options: true,
+//             },
+//         });
 
-        prisma.$disconnect();
+//         prisma.$disconnect();
         
         
-        return NextResponse.json({qustions: data}); 
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return NextResponse.error();
-    }
-}
+//         return NextResponse.json({qustions: data}); 
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//         return NextResponse.error();
+//     }
+// }
 
