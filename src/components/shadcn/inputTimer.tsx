a448ac7c4d82cd530d5e139167e4f2d184b31f9b
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2024-05-01 12:04:22 -0400

    add timer

--- src/components/shadcn/inputTimer.tsx
+++ src/components/shadcn/inputTimer.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import { cn } from "../../lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
-import { CountdownCircleTimer } from 'react-countdown-circle-timer'
-import { isValid } from "zod";
+
 
 
 
