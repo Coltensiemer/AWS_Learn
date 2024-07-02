c260715fb942d8e0ed53c6e02fbe66044397a7ee
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2024-07-02 16:09:20 -0400

    fixed build issue under Toast

--- src/components/Toasts.tsx
+++ src/components/Toasts.tsx
@@ -1,17 +1,8 @@
 "use client"
 
-<<<<<<<< HEAD:src/useClient/Toasts.tsx
-<<<<<<< HEAD:src/useClient/Toasts.tsx
-import { Toaster } from '../components/shadcn/toaster';
-import { Button } from '../components/shadcn/button/button';
-=======
-import { Toaster } from '../atomic/toaster';
-import { Button } from '../atomic/button/button';
->>>>>>> 6e60b80 (updated folder structure from shadcn to atomoic):src/components/useClient/Toasts.tsx
-========
 import { Toaster } from './atomic/toaster';
 import { Button } from './atomic/button/button';
->>>>>>>> 1c7c77e (updated component structure and route direction):src/components/Toasts.tsx
+
 
 export default function SuccessfulToast() {
 //@ts-ignore
