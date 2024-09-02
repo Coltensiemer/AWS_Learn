4c4cd3628b28d1b3d1ddf95d468fb8ef2174d63f
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2024-09-02 18:18:57 -0400

    updated seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,90 +1,89 @@
 import fs from 'fs'; // File system module
 import prisma from './prisma';
 
-
 /// This creates a user with the email for seed data
-async function main() {
-  try {
-    //Delete all users to clear the database
-    await prisma.user.deleteMany();
-    await prisma.fakeuser.deleteMany();
-    await prisma.completedquiz.deleteMany();
-    await prisma.quizprogress.deleteMany();
+export async function main() {
+	try {
+		//Delete all users to clear the database
+		await prisma.user.deleteMany();
+		await prisma.fakeuser.deleteMany();
+		await prisma.completedquiz.deleteMany();
+		await prisma.quizprogress.deleteMany();
 
-    await prisma.user.upsert({
-      where: { email: 'alice@prisma.io' },
-      update: {},
-      create: {
-        email: 'alice@prisma.io',
-        name: 'Alice',
-        password: 'password',
-        completedquiz: {
-          create: {
-            quizidused: [19, 20, 21, 22, 23, 24, 25, 26, 27],
-            correctanswers: [19, 21, 23],
-            incorrectanswers: [24, 25, 26],
-            tags: ['EC2', 'Lambda'],
-            score: 50,
-            starttimer: 120,
-            finishedat: 20,
-            quizDate: new Date(),
-            quizselectedoptions: [
-              { id: 19, value: 'A' },
-              { id: 20, value: 'B' },
-              { id: 21, value: 'C' },
-              { id: 22, value: 'D' },
-              { id: 23, value: 'A' },
-              { id: 24, value: 'B' },
-            ],
-          },
-        },
-        quizprogress: {
-          create: {
-            quizidused: [1, 2, 3, 4, 5, 6],
-            correctanswers: [1, 3],
-            incorrectanswers: [2, 4],
-            currentquestion: 5,
-            quiztimer: 42,
-            tags: ['EC2', 'Lambda'],
-          },
-        },
-      },
-    });
+		await prisma.user.upsert({
+			where: { email: 'alice@prisma.io' },
+			update: {},
+			create: {
+				email: 'alice@prisma.io',
+				name: 'Alice',
+				password: 'password',
+				completedquiz: {
+					create: {
+						quizidused: [19, 20, 21, 22, 23, 24, 25, 26, 27],
+						correctanswers: [19, 21, 23],
+						incorrectanswers: [24, 25, 26],
+						tags: ['EC2', 'Lambda'],
+						score: 50,
+						starttimer: 120,
+						finishedat: 20,
+						quizDate: new Date(),
+						quizselectedoptions: [
+							{ id: 19, value: 'A' },
+							{ id: 20, value: 'B' },
+							{ id: 21, value: 'C' },
+							{ id: 22, value: 'D' },
+							{ id: 23, value: 'A' },
+							{ id: 24, value: 'B' },
+						],
+					},
+				},
+				quizprogress: {
+					create: {
+						quizidused: [1, 2, 3, 4, 5, 6],
+						correctanswers: [1, 3],
+						incorrectanswers: [2, 4],
+						currentquestion: 5,
+						quiztimer: 42,
+						tags: ['EC2', 'Lambda'],
+					},
+				},
+			},
+		});
 
-    //Delete all quizzes, options and questions to clear the database
-    await prisma.option.deleteMany();
-    await prisma.quiz.deleteMany();
+		//Delete all quizzes, options and questions to clear the database
+		await prisma.option.deleteMany();
+		await prisma.quiz.deleteMany();
 
-    const jsonData = fs.readFileSync('src/Data.ts', 'utf8');
-    const data = JSON.parse(jsonData);
+		const jsonData = fs.readFileSync('src/Data.ts', 'utf8');
+		const data = JSON.parse(jsonData);
 
-    for (const item of data) {
-      await prisma.quiz.create({
-        data: {
-          tag: item.tag,
-          sub_tag: item.sub_tag,
-          question: item.question,
-          correct_answer: item.correct_answer,
-          options: {
-            create: item.options.map((option: any) => ({
-              value: option.value,
-              iscorrect: option.isCorrect, // Set isCorrect based on the value field of each option
-            })),
-          },
-        },
-      });
-    }
-  } catch (error) {
-    throw error;
-  }
+		for (const item of data) {
+			await prisma.quiz.create({
+				data: {
+					tag: item.tag,
+					sub_tag: item.sub_tag,
+					question: item.question,
+					correct_answer: item.correct_answer,
+					options: {
+						create: item.options.map((option: any) => ({
+							value: option.value,
+							iscorrect: option.isCorrect, // Set isCorrect based on the value field of each option
+						})),
+					},
+				},
+			});
+		}
+	} catch (error) {
+		throw error;
+	}
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
